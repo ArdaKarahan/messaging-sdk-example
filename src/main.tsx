bd94f8dc73135b7b149d4afe13820283fb6fc8b6
Daniel Leavitt <daniel.leavitt@mystenlabs.com>
2025-09-23 17:29:33 -0700

    feat: add slush web wallet support

--- src/main.tsx
+++ src/main.tsx
@@ -16,7 +16,12 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <Theme appearance="dark">
       <QueryClientProvider client={queryClient}>
         <SuiClientProvider networks={networkConfig} defaultNetwork="testnet">
-          <WalletProvider autoConnect>
+          <WalletProvider
+            autoConnect
+            slushWallet={{
+              name: "Messaging SDK Example App"
+            }}
+          >
             <App />
           </WalletProvider>
         </SuiClientProvider>
